3692487b9fdb2f7493657b27129b2897450b46dc
hungdmph36281 <hungdmph36281@fpt.edu.vn>
2023-12-11 00:05:17 +0700

    feat:bieudo

--- admin/thongke/bieudo.php
+++ admin/thongke/bieudo.php
@@ -1,3 +1,58 @@
+<style>  .content-page {
+    background-color: #f8f9fa;
+    padding: 20px;
+    font-family: 'Arial', sans-serif;
+  }
+
+  .container-fluid {
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+
+  .page-title-box {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+
+  h3.page-title {
+    text-align: center;
+    font-size: 24px;
+    color: #343a40;
+  }
+
+  #myChart {
+    width: 100%;
+    height: 300px;
+    margin: 0 auto;
+  }
+
+  /* Thêm các điều chỉnh màu sắc và font chữ cho biểu đồ */
+  .chartjs-render-monitor {
+    font-family: 'Arial', sans-serif;
+  }
+
+  /* Thêm điều chỉnh màu sắc cho các thanh xấp xỉ */
+  .chartjs-render-monitor .bar {
+    background-color: rgba(75, 192, 192, 0.8);
+  }
+
+  .chartjs-render-monitor .line {
+    background-color: rgba(75, 192, 192, 0.8);
+  }
+
+  /* Thêm một chút đổ bóng cho biểu đồ */
+  .chartjs-render-monitor .bar {
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+
+  /* Thêm kiểu hover cho thanh xấp xỉ */
+  .chartjs-render-monitor .bar:hover {
+    background-color: rgba(75, 192, 192, 1);
+  }
+</style>
 <div class="content-page">
   <div class="content">
 
@@ -10,56 +65,54 @@
           <div class="page-title-box">
             <div class="page-title-right">
               <ol class="breadcrumb m-0">
-                <li class="breadcrumb-item"><a href="index.php">Trang chủ admin</a></li>
-                <li class="breadcrumb-item active">Danh sách sản phẩm</li>
+                <!-- <li class="breadcrumb-item"><a href="index.php">Trang chủ admin</a></li>
+                <li class="breadcrumb-item active">Danh sách sản phẩm</li> -->
               </ol>
             </div>
-            <h3 class="page-title">BIỂU ĐỒ</h3>
-
-
-            <div class="container-fluid">
-              <div id="piechart"></div>
-            </div>
-
-            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
-            <script type="text/javascript">
-              // Load google charts
-              google.charts.load('current', {
-                'packages': ['corechart']
-              });
-              google.charts.setOnLoadCallback(drawChart);
-
-              // Draw the chart and set the chart values
-              function drawChart() {
-                var data = google.visualization.arrayToDataTable([
-                  ['Task', 'Hours per Day'],
-                  <?php
-                  $tongdm = count($listthongke);
-                  $i = 1;
-                  foreach ($listthongke as $thongke) {
-                    extract($thongke);
-                    if ($i == $tongdm) $dauphay = "";
-                    else $dauphay = ",";
-                    echo "['" . $thongke['tendm'] . "', " . $thongke['countsp'] . "],";
-                    $i += 1;
-                  }
-                  ?>
-                ]);
-
-                // Optional; add a title and set the width and height of the chart
-                var options = {
-                  'width': 1270,
-                  'height': 550
-                };
-
-                // Display the chart inside the <div> element with id="piechart"
-                var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-                chart.draw(data, options);
-              }
-            </script>
           </div>
+          <div>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+          </div>
+          <div>
+            <canvas id="myChart"></canvas>
+          </div>
+          <script>
+            // Đoạn mã JavaScript để tạo biểu đồ
+            document.addEventListener('DOMContentLoaded', function() {
+              var ctx = document.getElementById('myChart').getContext('2d');
 
+              // Dữ liệu mẫu cho biểu đồ (điểm của đồ thị đường)
+              var data = {
+                labels: ['Áo Nam', 'Quần Nam', 'Phụ Kiện'],
+                datasets: [{
+                  label: 'Số lượng',
+                  backgroundColor: 'rgba(55, 102, 102, 0.2)',
+                  borderColor: 'rgba(55, 102, 102, 1)',
+                  borderWidth: 2,
+                  data: [6, 5, 4]
+                }]
+              };
+
+
+              // Cấu hình cho biểu đồ
+              var options = {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                  y: {
+                    beginAtZero: true
+                  }
+                }
+              };
+
+              // Tạo biểu đồ đường
+              var myChart = new Chart(ctx, {
+                type: 'bar', // Loại biểu đồ: 'line', 'bar', 'radar', 'doughnut',...
+                data: data,
+                options: options
+              });
+            });
+          </script>
 
         </div>
       </div>
